990f80e8d12ae3940b9491788b84a6c3b4d4c353
weslley41 <41weslley@gmail.com>
2022-12-30 17:42:58 -0300

    add themes

---
+++ changeTheme.js
@@ -0,0 +1,23 @@
+
+function changeTheme() {
+    if (themeIndex >= 2) {
+        themeIndex = 0;
+    } else {
+        themeIndex++;
+    }
+
+    const themesList = ['brasil', 'light', 'dark'];
+    setTheme(themesList[themeIndex]);
+}
+
+function setTheme(theme) {
+    document.documentElement.classList = theme;
+}
+
+let prefersColor = window.matchMedia('(prefers-color-scheme: light)')
+        .matches ? 'brasil' : 'dark'
+setTheme(prefersColor)
+
+var themeIndex = prefersColor === 'brasil' ? 0 : 2;
+
+document.addEventListener('click', changeTheme);
