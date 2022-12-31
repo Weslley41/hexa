9ab5e634b10db67af0a7a129860650a39907391a
weslley41 <41weslley@gmail.com>
2022-12-31 11:06:18 -0300

    opção de iniciar com o contador para a abertura da copa

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var dates = {
-	'copa': new Date(2026, 5, 8),
+	'abertura': new Date(2026, 5, 8),
 	'hexa': new Date(2026, 6, 3),
 };
 var choice = 'hexa';
 var date;
 
 function changeCounter(newChoice) {
-	document.getElementById("counter-" + choice).classList.remove('active');
-	document.getElementById("counter-" + newChoice).classList.add('active');
+	document.getElementById(choice).classList.remove('active');
+	document.getElementById(newChoice).classList.add('active');
 	choice = newChoice;
 }
 
@@ -30,3 +30,6 @@ function setTimes(totalDays) {
 	document.getElementById("choiceCounter").innerText = choice === 'hexa' ? 'o Hexa!' : `a ${choice}!`;
 	document.getElementById("totalDaysValue").innerText = totalDays == 1 ? totalDays + ' dia' : totalDays + ' dias';
 }
+
+document.querySelector(window.location.hash).click()
+document.click()
